bcd017e8fca816fa50bed6187c275201fdbedb9f
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-02-21 09:40:49 +0700

    ✏️ Code updated

--- Module 1 - Face Recognition/face_recognition.py
+++ Module 1 - Face Recognition/face_recognition.py
@@ -10,6 +10,7 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 
 def detect(gray, frame):
@@ -21,11 +22,16 @@ def detect(gray, frame):
         roi_gray = gray[y: y + h, x: x + w]
         roi_color = frame[y: y + h, x: x + w]
         
-        eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=3)
+        eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=22)
         
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), thickness=2)
-    
+        
+        smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=22)
+        
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (0, 0, 255), thickness=2)
+        
     return frame
 
 
